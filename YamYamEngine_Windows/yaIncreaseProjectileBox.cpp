5e1be90102fc97082752b47da784d2c33d40687b
pcb <chanbb1950@gmail.com>
2023-10-12 20:49:03 +0900

    박찬빈 : 투사체 증가 박스 추가(작업 진행 없음, 추가만 한 상태)

---
+++ YamYamEngine_Windows/yaIncreaseProjectileBox.cpp
@@ -0,0 +1,48 @@
+#include "yaIncreaseProjectileBox.h"
+#include "yaplayer.h"
+#include "yaMeshRenderer.h"
+
+namespace ya
+{
+	IncreaseProjectileBox::IncreaseProjectileBox()
+	{
+	}
+	IncreaseProjectileBox::~IncreaseProjectileBox()
+	{
+	}
+	void IncreaseProjectileBox::Initialize()
+	{
+		RewardBox::Initialize();
+
+		MeshRenderer* mr = GetComponent<MeshRenderer>();
+		mr->SetColor(Vector3(0, 255, 0));
+	}
+	void IncreaseProjectileBox::Update()
+	{
+		RewardBox::Update();
+	}
+	void IncreaseProjectileBox::LateUpdate()
+	{
+		RewardBox::LateUpdate();
+	}
+	void IncreaseProjectileBox::Render()
+	{
+		RewardBox::Render();
+	}
+	void IncreaseProjectileBox::OnCollisionEnter(Collider* other)
+	{
+		RewardBox::OnCollisionEnter(other);
+	}
+	void IncreaseProjectileBox::OnCollisionStay(Collider* other)
+	{
+		RewardBox::OnCollisionStay(other);
+	}
+	void IncreaseProjectileBox::OnCollisionExit(Collider* other)
+	{
+		RewardBox::OnCollisionExit(other);
+	}
+
+	void IncreaseProjectileBox::ReturnReward(player* target)
+	{
+	}
+}
