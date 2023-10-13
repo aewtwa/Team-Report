cdc634da7466898abc5272ba321475036643d110
pcb <chanbb1950@gmail.com>
2023-10-13 22:08:52 +0900

    박찬빈 : 텍스트 렌더 오류 수정

--- YamYamEngine_SOURCE/yaCamera.cpp
+++ YamYamEngine_SOURCE/yaCamera.cpp
@@ -8,16 +8,15 @@ extern ya::Application application;
 
 namespace ya
 {
-	Vector3 Camera::mResolution = Vector3::Zero;
+	Vector2 Camera::mResolution = Vector2::Zero;
 	Vector3 Camera::mLookPosition = Vector3::Zero;
 	Vector3 Camera::mDistance = Vector3::Zero;
+	Vector2 Camera::mDistanceApi = Vector2::Zero;
 	GameObject* Camera::mTarget = nullptr;
 
 	void Camera::Initialize()
 	{
-		mResolution.x = application.GetWidth();
-		mResolution.y = application.GetHeight();
-		mLookPosition = mResolution / 2.0f;
+		Clear();
 	}
 	void Camera::Update()
 	{
@@ -31,11 +30,12 @@ namespace ya
 		}
 
 		mDistance = mLookPosition;
+		mDistanceApi = mLookPosition - (mResolution / 2);
 	}
 	void Camera::Clear()
 	{
 		mResolution.x = application.GetWidth();
 		mResolution.y = application.GetHeight();
-		mLookPosition = mResolution / 2.0f;
+		mLookPosition = (Vector3)mResolution / 2.0f;
 	}
 }
